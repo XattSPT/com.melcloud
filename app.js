16782436e2eb60bd3752e0ae61194db80a88ea11
Olivier Zalmanski <olivier.zalmanski@me.com>
2022-09-13 22:50:55 +0200

    Clean code,

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const Homey = require('homey');
 
 class MelCloud extends Homey.App {
-    onInit() {
-        console.log('Successfully init MelCloud version: %s', Homey.app.manifest.version);
 
+  onInit() {
+    console.log('Successfully init MelCloud version: %s', Homey.app.manifest.version);
+  };
 
-    };
 }
 
 module.exports = MelCloud;
